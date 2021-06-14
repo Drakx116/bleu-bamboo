c33e48ffb7299ec1ab71b92be2d84eab827562a1
Drakx116 <alexandre2.guyot@gmail.com>
2021-06-14 15:50:33 +0200

    Product - Add items in listing page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
+import commerce from "../lib/commerce";
+
 const Products = () => {
+  const [ products, setProducts ] = useState([]);
+  const [ loaded, setLoaded ] = useState(false);
+
+  useEffect(() => {
+    commerce.products.list()
+      .then(products => {
+        setLoaded(true);
+        return setProducts(products);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+
+
   return (
-    <h1> Products </h1>
+    <div>
+      <h1> Bleu Bamboo </h1>
+      { loaded ? 'OK': 'Loading ...' }
+    </div>
   );
 }
 
