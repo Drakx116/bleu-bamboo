212e4393f7c916640507e304be9f47059d7e541f
Drakx116 <alexandre2.guyot@gmail.com>
2021-06-14 15:30:05 +0200

    Router - Add router instance with homepage and products pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+
 function App() {
   return (
-    <div className="main">
-      <h1> Blue Bamboo </h1>
-    </div>
+    <main>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/products" component={ Products } />
+      </Switch>
+    </main>
   );
 }
 
