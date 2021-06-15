1f8f3b135a917bc7e32456cd08c9e6c7fef94bb1
Drakx116 <alexandre2.guyot@gmail.com>
2021-06-15 14:46:19 +0200

    Style - Add CSS reset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Route, Switch } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+
+import './assets/scss/_reset.scss';
 
 function App() {
   return (
