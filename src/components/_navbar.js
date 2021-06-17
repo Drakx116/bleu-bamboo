c7905c01e60cc1baa7586131996dbbefb60b8d7d
Drakx116 <alexandre2.guyot@gmail.com>
2021-06-17 13:34:54 +0200

    Navbar - Handle responsive

--- src/components/_navbar.js
+++ src/components/_navbar.js
@@ -1,16 +1,44 @@
+import { useState } from "react";
+
+import { toggle, handleResize } from "../assets/js/navbar";
+
 import logo from '../assets/images/rounded-logo.png';
-import cart from '../assets/images/svg/user.svg';
-import user from '../assets/images/svg/cart.svg';
+import cart from '../assets/images/svg/cart.svg';
+import close from '../assets/images/svg/close.svg';
+import hamburger from '../assets/images/svg/hamburger.svg';
+import heart from '../assets/images/svg/heart.svg';
+import user from '../assets/images/svg/user.svg';
 
 import '../assets/scss/_navbar.scss';
 
 const Navbar = () => {
+  window.onresize = () => {
+    if (handleResize(isOpened)) {
+      setOpened(false);
+    }
+  }
+
+  const [ isOpened, setOpened ] = useState(false);
+
+  const toggleNavbar = () => {
+    toggle(isOpened);
+
+    return setOpened(!isOpened);
+  }
+
   return (
     <nav id="navbar">
       <div id="navbar-logo">
         <a href="/">
           <img src={ logo } alt="Bleu Bamboo"/>
         </a>
+        <span className="navbar-action" id="opened" onClick={ () => toggleNavbar() }>
+          <span id="hamburger-label"> Menu </span>
+          <img className="navbar-toggle" id="hamburger" src={ hamburger } alt=""/>
+        </span>
+        <span className="navbar-action" id="closed" onClick={ () =>  toggleNavbar() }>
+          <img className="navbar-toggle" id="arrow-close" src={ close } alt=""/>
+        </span>
       </div>
       <div id="navbar-container">
         <div className="navbar-content">
@@ -22,6 +50,12 @@ const Navbar = () => {
         </div>
         <div className="navbar-content">
           <ul>
+            <li>
+              <a href="#">
+                <img src={ heart } alt="Wishlist"/>
+                <span> Wishlist </span>
+              </a>
+            </li>
             <li>
               <a href="#">
                 <img src={ cart } alt="Icône panier"/>
