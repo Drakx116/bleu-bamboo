28b0cfcbe3354ea03f9e569a5ba30838629f1a72
Drakx116 <alexandre2.guyot@gmail.com>
2021-06-19 16:18:36 +0200

    Homepage - Add comparision slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,37 @@
+import DirtyToClean from "../components/Comparision/DirtyToClean";
+import Button from "../components/Global/Button";
+
+import '../assets/scss/_homepage.scss';
+
 const Home = () => {
   return (
-    <h1> Bleu Bamboo </h1>
+    <div id="container">
+      <DirtyToClean />
+      <div id="jumbotron">
+        <span id="jumbotron-title"><span className="light">Voyager sans impact pour l'environnement, </span> c'est possible !</span>
+        <a href="/products">
+          <Button text="Je découvre les produits" marginTop={ "2em" } />
+        </a>
+        <span id="jumbotron-summary">
+          <span id="header"> Depuis notre création : </span>
+          <div id="summary-data">
+            <div className="item">
+              <span className="number"> 10 256</span>
+              <span className="text"> Kilos de déchets qui ont été évités </span>
+            </div>
+            <div className="item">
+              <span className="number"> 60 </span>
+              <span className="text"> Personnes qui ont adopté le kit </span>
+            </div>
+            <div className="item">
+              <span className="number"> 6 000 </span>
+              <span className="text"> Mètres carrés de littoraux dépollués </span>
+            </div>
+          </div>
+        </span>
+      </div>
+    </div>
   );
-}
+};
 
 export default Home;
