8c7936a886fedbaf502347e79793c5dcde6ff23b
Drakx116 <alexandre2.guyot@gmail.com>
2021-06-24 23:02:38 +0200

    Add Loading animation

--- src/components/Global/Loading.js
+++ src/components/Global/Loading.js
@@ -1,7 +1,12 @@
+import loading from '../../assets/images/loading.gif';
+
+import '../../assets/scss/global/_loading.scss';
+
 const Loading = () => {
   return (
-    <div className="container">
-      Loading ...
+    <div id="loading">
+      <span> Encore un instant, le contenu arrive ... </span>
+      <img src={ loading } alt="GIF de chargement"/>
     </div>
   );
 }
